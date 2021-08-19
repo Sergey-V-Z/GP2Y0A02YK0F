12f944fe17e10c317c82fe61408e19e464a274e7
Sergey <skiner_1988@mail.ru>
2021-08-19 11:56:59 +0800

    Setings
    Закончил настройки проекта

--- f303_sens/EWARM/settings/f303_sens.f303_sens.cspy.ps1
+++ f303_sens/EWARM/settings/f303_sens.f303_sens.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Radio\Jobe\IR_Controller\f303\f303_sens\EWARM\settings\f303_sens.f303_sens.general.xcl" --backend -f "D:\Radio\Jobe\IR_Controller\f303\f303_sens\EWARM\settings\f303_sens.f303_sens.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Kicads Schem\GP2Y0A02YK0F\f303_sens\EWARM\settings\f303_sens.f303_sens.general.xcl" --backend -f "D:\Kicads Schem\GP2Y0A02YK0F\f303_sens\EWARM\settings\f303_sens.f303_sens.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Radio\Jobe\IR_Controller\f303\f303_sens\EWARM\settings\f303_sens.f303_sens.general.xcl" --debug_file=$debugfile --backend -f "D:\Radio\Jobe\IR_Controller\f303\f303_sens\EWARM\settings\f303_sens.f303_sens.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Kicads Schem\GP2Y0A02YK0F\f303_sens\EWARM\settings\f303_sens.f303_sens.general.xcl" --debug_file=$debugfile --backend -f "D:\Kicads Schem\GP2Y0A02YK0F\f303_sens\EWARM\settings\f303_sens.f303_sens.driver.xcl" 
 }
